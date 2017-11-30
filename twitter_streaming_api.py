543247ed82202f39ec8631126baae02d823ded82
Lee Long Hui <leelonghui3@gmail.com>
2017-11-30 12:30:34 +0000

    in the midst of fixing the truncated tweets error

--- twitter_streaming_api.py
+++ twitter_streaming_api.py
@@ -1,70 +1,101 @@
-import twitter, json, sys, csv
+'''Tweet Streaming API consumer'''
+import twitter
+import csv
 
 # == OAuth Authentication ==
-consumer_key="GXbUDGfeYjHGuURCvmBTpbBqS"
-consumer_secret="VFDJZaO0fL1VXcA7zQbPahBqokyqyUOzARaL3l0SBeN0Nif3en"
-access_token="113197842-xRrss0ZJbyiUq5eV4fyd0Mf0bRHUHvoCTAAtX2SE"
-access_token_secret="NagevuIxsC2r3T33e4C5Yof6kPWxMKDbDtNlK50mCrmil"
-
-auth = twitter.oauth.OAuth(access_token, access_token_secret, consumer_key, consumer_secret)
-twitter_api = twitter.Twitter(auth=auth)
-
-csvfile = open('kltu_data.csv', 'w')
-csvwriter = csv.writer(csvfile)
-csvwriter.writerow([['created_at'],
-					['screen_name'],
-					['text'],
-					['created_at'],
-					['followers_count'],
-					['friends_count'],
-					['statuses_count'],
-					['source'],
-					['location'],
-					['geo_enabled'],
-					['lang'],
-					['time_zone'],
-					['retweet_count']
-					])
+consumer_key="HUcXih4iRVgoyV13IqxRlS5BU"
+consumer_secret="QUcm0AWx75ajw5Ewqepu4b43pXw1QdswpTrCl9VPcvodIPx2jZ"
+access_token="113197842-iicH3rBNj41yzehS4ke3Ft5BiZW3vcelQj40BVnl"
+access_token_secret="ijBDkBNxmkVy0JtLsyVaVZOzzIk3KVaD7uX9XB7Tx8x8l"
 
 
-q = "kltu"
+AUTH = twitter.oauth.OAuth(access_token, access_token_secret, consumer_key, consumer_secret)
+TWITTER_API = twitter.Twitter(auth=AUTH)
 
-# small function encapsuate the encodes and protect replace nulls with empty values
+csvfile = open('starwars.csv', 'w')
+csvwriter = csv.writer(csvfile,delimiter = '|')
+csvwriter.writerow(['created_at',
+                    'user-id',
+                    'user-screen_name',
+                    'text',
+                    'coordinates lng',
+                    'coordinates lat',
+                    'place',
+                    'user-location',
+                    'user-geo_enabled',
+                    'user-lang',
+                    'user-time_zone',
+                    'user-statuses_count',
+                    'user-followers_count',
+                    'user-friends_count',
+                    'user-created_at',
+                    'user-source'])
+
+q = "starwars"
+
+# clean up our data so we can write unicode to CSV
 def clean(val):
-	clean = ""
-	if val:
-		clean = val.encode('utf-8')
-	return clean
-
+    clean = ""
+    if val:
+        val = val.replace('|', ' ')
+        val = val.replace('\n', ' ')
+        val = val.replace('\r', ' ')
+        clean = val.encode('utf-8')
+    return clean
 
 print 'Filtering the public timeline for keyword="%s"' % (q)
-twitter_stream = twitter.TwitterStream(auth=twitter_api.auth)
+twitter_stream = twitter.TwitterStream(auth=TWITTER_API.auth)
 stream = twitter_stream.statuses.filter(track=q)
 
-for tweet in stream:
- 	# print json.dumps(tweet)
-    try:
-        csvwriter.writerow([tweet['created_at'],
-                            clean(tweet['user']['screen_name']),
-                            clean(tweet['text']),
-                            tweet['user']['created_at'],
-                            tweet['user']['followers_count'],
-                            tweet['user']['friends_count'],
-                            tweet['user']['statuses_count'],
-                            clean(tweet['source']),
-                            clean(tweet['user']['location']),
-                            tweet['user']['geo_enabled'],
-                            tweet['user']['lang'],
-                            clean(tweet['user']['time_zone']),
-                            tweet['retweet_count']
-                            ])
 
-        print tweet['user']['screen_name'].encode('utf-8'), tweet['text'].encode('utf-8')
+''' helper functions, clean data, unpack dictionaries '''
+def getVal(val):
+    clean = ""
+    if isinstance(val, bool):
+        return val
+    if isinstance(val, int):
+        return val
+    if val:
+        clean = val.encode('utf-8')
+    return clean
+
+def getLng(val):
+    if isinstance(val, dict):
+        return val['coordinates'][0]
+
+def getLat(val):
+    if isinstance(val, dict):
+        return val['coordinates'][1]
+
+def getPlace(val):
+    if isinstance(val, dict):
+        return val['full_name'].encode('utf-8')
+
+
+# main loop
+for tweet in stream:
+    try:
+        if tweet['truncated']:
+            tweet_text = tweet['extended_tweet']['full_text']
+		else:
+			tweet_text = tweet['text']
+			csvwriter.writerow([tweet['created_at'],
+								getVal(tweet['user']['id']),
+								getVal(tweet['user']['screen_name']),
+								getVal(tweet['text']),
+								getLng(tweet['coordinates']),
+								getLat(tweet['coordinates']),
+								getPlace(tweet['place']),
+								getVal(tweet['user']['location']),
+								getVal(tweet['user']['geo_enabled']),
+								getVal(tweet['user']['lang']),
+								getVal(tweet['user']['time_zone']),
+								getVal(tweet['user']['statuses_count']),
+								getVal(tweet['user']['followers_count']),
+								getVal(tweet['user']['friends_count']),
+								getVal(tweet['user']['created_at']),
+								getVal(tweet['source'])])
+        csvfile.flush()
+        print getVal(tweet['user']['screen_name']), getVal(tweet['text']), tweet['coordinates'], getPlace(tweet['place'])
     except Exception as e:
         print e.message
-        # just print the error and keep going! we don't care if we lose a few tweets due to errors
-        # in a production version of this code we would write the errors into seperate file
-        pass
-
-
-print "done"
