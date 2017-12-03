0e5c87e6118134a76c06df5b90be12d39ccada2f
Lee Long Hui <leelonghui3@gmail.com>
2017-12-03 22:21:20 +0000

    please help get me to get original retweeted text into the csv file.

--- twitter_streaming_api.py
+++ twitter_streaming_api.py
@@ -1,5 +1,5 @@
 '''Tweet Streaming API consumer'''
-import twitter, csv, json
+import twitter, csv, json, sys
 
 # == OAuth Authentication ==
 consumer_key="HUcXih4iRVgoyV13IqxRlS5BU"
@@ -14,46 +14,44 @@ twitter_api = twitter.Twitter(auth=auth)
 csvfile = open('starwars.csv', 'w')
 csvwriter = csv.writer(csvfile,delimiter ='|')
 
-# csvwriter.writerow(['created_at',
-#                     'user-screen_name',
-#                     'text',
-#                     'coordinates lng',
-#                     'coordinates lat',
-#                     'place',
-#                     'user-location',
-#                     'user-geo_enabled',
-#                     'user-lang',
-#                     'user-time_zone',
-#                     'user-statuses_count',
-#                     'user-followers_count',
-#                     'user-friends_count',
-#                     'user-created_at',
-#                     'user-source'])
+csvwriter.writerow(['created_at',
+                    'user-id',
+                    'user-screen_name',
+                    'text',
+                    'tweet-id',
+                    'tweet-place',
+                    'user-created_at',
+                    'user-followers_count',
+                    'user-friends_count',
+                    'user-statuses_count',
+                    'user-source',
+                    'user-time_zone',
+                    'user-geo_enabled',
+                    'coordinates lng',
+                    'coordinates lat',
+                    'user-location',
+                    'user-language',
+                    'retweeted_text',
+                    'retweeted_id'
+                    ])
 
 q = "starwars"
-
-# clean up our data so we can write unicode to CSV
-def clean(val):
-    clean = ""
-    if val:
-        val = val.replace('|', ' ')
-        val = val.replace('\n', ' ')
-        val = val.replace('\r', ' ')
-        clean = val.encode('utf-8')
-    return clean
 
 print 'Filtering the public timeline for keyword="%s"' % (q)
 twitter_stream = twitter.TwitterStream(auth=twitter_api.auth)
 stream = twitter_stream.statuses.filter(track=q)
 
 ''' helper functions, clean data, unpack dictionaries '''
-def getVal(val):
+def clean(val):
     clean = ""
     if isinstance(val, bool):
         return val
     if isinstance(val, int):
         return val
     if val:
+        val = val.replace('|', ' ')
+        val = val.replace('\n', ' ')
+        val = val.replace('\r', ' ')
         clean = val.encode('utf-8')
     return clean
 
@@ -71,31 +69,63 @@ def getPlace(val):
 
 for tweet in stream:
     # print json.dumps(tweet)
+    # sys.exit(1)
+
     try:
         if tweet['truncated']:
             tweet_text = tweet['extended_tweet']['full_text']
         else:
             tweet_text = tweet['text']
+
         csvwriter.writerow([tweet['created_at'],
+                            tweet['user']['id'],
                             clean(tweet['user']['screen_name']),
                             clean(tweet_text),
-                            getLng(tweet['coordinates']),
-                            getLat(tweet['coordinates']),
-                            getVal(tweet['user']['location']),
+                            tweet['id'],
+                            getPlace(tweet['place']),
                             tweet['user']['created_at'],
                             tweet['user']['followers_count'],
                             tweet['user']['friends_count'],
                             tweet['user']['statuses_count'],
                             clean(tweet['source']),
-                            clean(tweet['user']['location']),
+                            clean(tweet['user']['time_zone']),
                             tweet['user']['geo_enabled'],
+                            getLng(tweet['coordinates']),
+                            getLat(tweet['coordinates']),
+                            clean(tweet['user']['location']),
                             tweet['user']['lang'],
-                            clean(tweet['user']['time_zone'])
+                            tweet['retweet_count']
                             ])
-        print tweet_text
+
+        if tweet['retweeted_status'] != {}:
+            retweeted_text = tweet['retweeted_status']['extended_tweet']['full_text']
+
+        csvwriter.writerow([tweet['created_at'],
+                            tweet['user']['id'],
+                            clean(tweet['user']['screen_name']),
+                            clean(tweet_text),
+                            tweet['id'],
+                            getPlace(tweet['place']),
+                            tweet['user']['created_at'],
+                            tweet['user']['followers_count'],
+                            tweet['user']['friends_count'],
+                            tweet['user']['statuses_count'],
+                            clean(tweet['source']),
+                            clean(tweet['user']['time_zone']),
+                            tweet['user']['geo_enabled'],
+                            getLng(tweet['coordinates']),
+                            getLat(tweet['coordinates']),
+                            clean(tweet['user']['location']),
+                            tweet['user']['lang'],
+                            tweet['retweet_count'],
+                            clean(retweeted_text),
+                            tweet['retweeted_status']['id']
+                            ])
+
+        print clean(tweet['user']['screen_name']), clean(tweet_text)
+
     except Exception, err:
         print err
         pass
-
 
 print "done"
